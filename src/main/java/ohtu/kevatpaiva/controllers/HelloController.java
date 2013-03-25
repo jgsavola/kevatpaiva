5b62af2a6518e51ee926fc302b35ce447b54dffc
Heidi Jauhiainen <wikgren@gmail.com>
2013-03-25 15:41:42 +0200

    Controlleria muokattu

--- src/main/java/ohtu/kevatpaiva/controllers/HelloController.java
+++ src/main/java/ohtu/kevatpaiva/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package ohtu.kevatpaiva.controllers;
 
+import ohtu.kevatpaiva.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -19,6 +21,22 @@ public class HelloController {
 
         model.addAttribute("message", "(I am spring)");
         return "hello";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String lisaaArtikkeli(@RequestParam String id, @RequestParam String author, 
+            @RequestParam String title, @RequestParam String journal, @RequestParam int year, 
+            @RequestParam int volume, @RequestParam int number, @RequestParam String pages, 
+            @RequestParam String publisher) {
+        
+        Article artikkeli = new Article(id, author, title, year);
+        artikkeli.setJournal(journal);
+        artikkeli.setVolume(volume);
+        artikkeli.setNumber(number);
+        artikkeli.setPages(pages);
+        artikkeli.setPublisher(publisher);
+        return null;
+        
     }
  
 }
