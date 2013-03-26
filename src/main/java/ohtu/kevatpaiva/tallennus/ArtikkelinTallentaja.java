3783aaf0da2ec9376160d977ab91c173af7f3a46
Heidi Jauhiainen <wikgren@gmail.com>
2013-03-26 16:04:05 +0200

    Tallentaja luokka lisättiin

---
+++ src/main/java/ohtu/kevatpaiva/tallennus/ArtikkelinTallentaja.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.kevatpaiva.tallennus;
+
+import java.util.Properties;
+import ohtu.kevatpaiva.Article;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ *
+ * @author hwikgren
+ */
+public class ArtikkelinTallentaja {
+    
+    private static SessionFactory sessionFactory = null; 
+    private static ServiceRegistry serviceRegistry = null; 
+    
+    private static Session session = null;
+    Transaction tx=null;
+
+    public ArtikkelinTallentaja() {
+        configureSessionFactory();
+        
+         
+        session = sessionFactory.openSession();
+    }
+    
+    
+    private static void configureSessionFactory() throws HibernateException { 
+        Configuration configuration = new Configuration(); 
+        configuration.configure(); 
+         
+        Properties properties = configuration.getProperties();
+         
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(properties).buildServiceRegistry();         
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry); 
+         
+        //return sessionFactory; 
+    }
+    
+    public void tallennaArtikkeli(Article artikkeli) {
+        try {
+            tx = session.beginTransaction();
+             
+            // Saving to the database
+            session.save(artikkeli);
+             
+            // Committing the change in the database.
+            session.flush();
+            tx.commit();
+        }
+        finally {
+            
+        }
+    }
+}
