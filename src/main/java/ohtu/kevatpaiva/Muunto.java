d5d7d971af12dd806d3a27c30c362e85e5b5c5c4
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2013-04-16 11:57:03 +0300

    Korjaa skandien käsittettely Muunto-luokassa
    Toteuta uudelleen skandien ja isojen kirjainten suojaaminen käyttäen
    säännöllisiä lausekkeita ja HashMap-luokkaa.

--- src/main/java/ohtu/kevatpaiva/Muunto.java
+++ src/main/java/ohtu/kevatpaiva/Muunto.java
@@ -1,49 +1,93 @@
 package ohtu.kevatpaiva;
 
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
+ * Luokka BibTeX-kenttien muodon käsittelyyn.
+ *
+ * Toistaiseksi toteutettu skandien koodaus BibTeX/LaTeX-muotoon ja
+ * isojen kirjainten suojaus.
  *
  * @author ninbarlu
+ * @author jgsavola
  */
 public class Muunto {
+    private static final Pattern isotKirjaimetPattern = Pattern.compile("([\\p{javaUpperCase}])");
+    private static final HashMap<String, String> skandiMap = new HashMap<String, String>();
 
-    private static String okt = "abcdefghijklmnopqrstuvxyz0123456789 .,-";
-    private static String isot = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-    private static String skandit = "åäöÅÄÖ";
-    private static String[] skmuunto = {"\\\"{o}","\\\"{a}","\\\"{o}","\\\"{O}","\\\"{A}","\\\"{O}"};
-    
+    /**
+     * Muunto-luokan staattinen alustus.
+     *
+     * Lisää tähän LaTex/BibTeX-muunnokset skandeille. (Toki kelpaavat
+     * muutkin merkit kuin skandit.
+     */
+    static {
+        skandiMap.put("å", "\\aa");
+        skandiMap.put("ä", "\\\"{a}");
+        skandiMap.put("ö", "\\\"{o}");
+        skandiMap.put("Å", "\\AA");
+        skandiMap.put("Ä", "\\\"{A}");
+        skandiMap.put("Ö", "\\\"{O}");
+    }
+
+    /**
+     * Muunna <code>merkkijono</code> BibTeX-muotoon.
+     *
+     * Jos <code>title</code> on <code>true</code>, tee tarvittavat
+     * muunnokset isoille kirjaimmille, kuten BibTeX:n "title"-kenttä vaatii.
+     *
+     * FIXME: Nyt isojen kirjainten suojaaminen suojaa otsikoissa myös
+     * isot skandit, joten skandien suojaamisen jälkeen molemmat suojaukset
+     * on käytössä (Ö ==> {Ö} ==> {\"{O}}). Onko tämä ok?
+     *
+     * @param merkkijono Muunnettava merkkijono.
+     * @param title Onko kyseessä otsikkokenttä ("title").
+     * @return Muunnettu merkkijono.
+     */
     public static String muunnaMuoto(String merkkijono, boolean title) {
-        
-        String tulos = new String();
-        String muunnos = new String();
-        int i;
-        for (i =0; i < merkkijono.length(); i++ ) {
-            char merkki = merkkijono.charAt(i);
-            if (okt.indexOf(merkki) > -1) {
-                muunnos = "" + merkki;
-            } else if (isot.indexOf(merkki) > -1) {
-                muunnos = muunnaIsot(merkki, title);
-            } else if (skandit.indexOf(merkki) > -1) {
-                muunnos = muunnaSkandit(merkki, skandit.indexOf(merkki));
-            } else
-                muunnos = muunnaMuut(merkki);
-            tulos += muunnos;
+        String tulos = merkkijono;
+
+        if (title) {
+            tulos = suojaaIsotKirjaimet(tulos);
         }
+
+        tulos = suojaaSkandit(tulos);
+
         return tulos;
     }
-    
-    private static String muunnaIsot(char merkki, boolean title) {
-        if (title) {
-            return "{" + merkki + "}";
-        } else
-            return "" + merkki;
-    }
-    
-    private static String muunnaSkandit(char merkki, int i) {
-        return skmuunto[i];
+
+    /**
+     * Suojaa isot kirjaimet aaltosuluilla.
+     *
+     * TODO: Joskus isoa kirjainta ei tarvitse suojata, esim. tekstikentän
+     * ensimmäistä kirjainta. Tämän voisi ehkä myöhemmin huomioida, jotta
+     * lopputulos olisi helpompi ihmisten käyttöön.
+     *
+     * @param merkkijono Muunnettava merkkijono.
+     * @return Muunnettu merkkijono.
+     */
+    private static String suojaaIsotKirjaimet(String merkkijono) {
+        Matcher m = isotKirjaimetPattern.matcher(merkkijono);
+        return m.replaceAll("{$1}");
     }
 
-    private static String muunnaMuut(char merkki) {
-        return "" + merkki;
-    }
+    /**
+     * Suojaa "skandit" yksi kerrallaan.
+     *
+     * Metodi käyttää staattista <code>skandiMap</code>-taulukkoa.
+     *
+     * @param merkkijono Suojattava merkkijono.
+     * @return Skandisuojattu merkkijono.
+     */
+    private static String suojaaSkandit(String merkkijono) {
+        String tulos = merkkijono;
 
+        for (String skandi : skandiMap.keySet()) {
+            tulos = tulos.replace(skandi, skandiMap.get(skandi));
+        }
+
+        return tulos;
+    }
 }
--- src/test/java/ohtu/kevatpaiva/MuuntoTest.java
+++ src/test/java/ohtu/kevatpaiva/MuuntoTest.java
@@ -18,7 +18,7 @@ public class MuuntoTest {
     public void muunnaIsotEiTitle() {
         assertEquals("teStI", Muunto.muunnaMuoto("teStI", false));
     }
-/*    
+
     @Test
     public void muunnaSkandit() {
         assertEquals("a\\\"{A}\\\"{o}O", Muunto.muunnaMuoto("aÄöO", false));
@@ -26,7 +26,10 @@ public class MuuntoTest {
     
     @Test
     public void muunnaKokoJono() {
-        assertEquals("{O}tsikko, \\\"{a}\\\"{a}kk\\\"{o}set", Muunto.muunnaMuoto("Otsikko, ääkköset", true));
+        String syote = "Otsikko, ääkköset";
+        String odotettu = "{O}tsikko, \\\"{a}\\\"{a}kk\\\"{o}set";
+
+        String tulos = Muunto.muunnaMuoto(syote, true);
+        assertEquals("Otsikkomerkkijono ääkkösillä muunnetaan oikein", odotettu, tulos);
     }
-*/    
 }
