1bc2cc2ade46b2ff3ec3bdda0f12a2fbf0b122a3
Daniel Lillqvist <daniel.lillqvist@helsinki.fi>
2013-04-17 23:14:39 +0300

    Kommenttisivous

--- src/main/java/ohtu/kevatpaiva/Kentta.java
+++ src/main/java/ohtu/kevatpaiva/Kentta.java
@@ -8,35 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-/**
- *
- *
- *     private String author;
-    private String title;
-    private String year;
-    private String address;
-    private String annote;
-    private String booktitle;
-    private String chapter;
-    private String crossref;
-    private String edition;
-    private String editor;
-    private String howpublished;
-    private String institution;
-    private String journal;
-    private String key;
-    private String month;
-    private String note;
-    private String number;
-    private String organization;
-    private String pages;
-    private String publisher;
-    private String school;
-    private String series;
-    private String type;
-    private String volume;
-    URL
- */
 @Entity
 @Table(name = "kentta")
 public class Kentta {
