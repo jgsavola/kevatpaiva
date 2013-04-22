fc76d3ef31c1a281f7fa7987cbee7be148df7094
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2013-04-22 17:51:54 +0300

    Lisää book- ja inproceedings-tyyppien vaatimat kentät Referencestä
    Lisää lisäksi viiteTyyppi-attribuutti, joka kertoo viitteen tyypin.
    
    Huom! Oletuksena se on nyt "article". Tämä pitää korjata tätä luokkaa
    käyttävässä koodissa.

--- src/main/java/ohtu/kevatpaiva/Article.java
+++ src/main/java/ohtu/kevatpaiva/Article.java
@@ -15,27 +15,54 @@ public class Article implements Serializable {
     
     @Id
     private String id;
+
+    /**
+     * Tämä attribuutti ei ole BibTeX-kenttä, vaan viitteen tyyppi!
+     */
+    private String viiteTyyppi;
+    
+    private String type;
     private String author;
     private String title;
-    private String journal;
-    private String volume;
-    private String number;
     private String year;
+    private String address;
+    private String annote;
+    private String booktitle;
+    private String chapter;
+    private String crossref;
+    private String edition;
+    private String editor;
+    private String howpublished;
+    private String institution;
+    private String journal;
+    private String key;
+    private String month;
+    private String note;
+    private String number;
+    private String organization;
     private String pages;
     private String publisher;
-    private String address;
- 
+    private String school;
+    private String series;
+    private String volume;
+    private String url;                     // nonstandard field
+
     public Article() {
- 
+        setViiteTyyppi("article");
     }
  
+    public Article(String viiteTyyppi) {
+        setViiteTyyppi(viiteTyyppi);
+    }
+    
     public Article(String id, String author, String title, String year) {
+        setViiteTyyppi("article");
+
         this.id = id;
         this.author = author;
         this.title = title;
         this.year = year;
     }
- 
     
     public String getId() {
         return this.id;
@@ -116,6 +143,149 @@ public class Article implements Serializable {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public String getViiteTyyppi() {
+        return viiteTyyppi;
+    }
+
+    public void setViiteTyyppi(String viiteTyyppi) {
+        if ("article".equals(viiteTyyppi) 
+            || "book".equals(viiteTyyppi) 
+            || "inproceedings".equals(viiteTyyppi)) {
+            this.viiteTyyppi = viiteTyyppi;
+        } else {
+            throw new IllegalArgumentException("Tuntematon viitetyyppi " + viiteTyyppi);
+        }
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAnnote() {
+        return annote;
+    }
+
+    public void setAnnote(String annote) {
+        this.annote = annote;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getCrossref() {
+        return crossref;
+    }
+
+    public void setCrossref(String crossref) {
+        this.crossref = crossref;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getHowpublished() {
+        return howpublished;
+    }
+
+    public void setHowpublished(String howpublished) {
+        this.howpublished = howpublished;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+ 
 
     /**
      * Luo BibTeX-esitys artikkelista. Lopputuloksena pitäisi olla
